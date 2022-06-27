b2ddbe41f76488ce3f5ed3bc08bb4c1da416f5c1
Brian Beltran <brianbeltran93@gmail.com>
2022-06-27 19:47:10 -0400

    Initial commit

---
+++ Database.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FitnessApp
+{
+    internal static class Database
+    {
+        internal static void Read(string workoutTable)
+        {
+            string constr;
+
+            SqlConnection conn;
+
+            constr = @"Data Source=localhost;Initial Catalog=WorkoutDatabase;User ID=sa;Password=t3ddy123";
+
+            conn = new SqlConnection(constr);
+
+            conn.Open();
+
+            SqlCommand cmd;
+
+            SqlDataReader dreader;
+
+            string sql, output = "";
+
+            sql = $"Select Name from {workoutTable} order by Name";
+
+            cmd = new SqlCommand(sql, conn);
+
+            dreader = cmd.ExecuteReader();
+
+            while (dreader.Read())
+            {
+                output = output + dreader.GetValue(0) + "\n";
+            }
+
+            Console.Write(output);
+
+            dreader.Close();
+            cmd.Dispose();
+
+            conn.Close();
+        }
+
+        internal static bool Insert(string workoutTable, string newWorkout)
+        {
+            string constr;
+
+            SqlConnection conn;
+
+            constr = @"Data Source=localhost;Initial Catalog=WorkoutDatabase;User ID=sa;Password=t3ddy123";
+
+            conn = new SqlConnection(constr);
+
+            conn.Open();
+
+            SqlCommand cmd;
+
+            SqlDataAdapter adap = new SqlDataAdapter();
+
+            string sql = "";
+
+            sql = $"insert into {workoutTable} values('{newWorkout}')";
+
+            cmd = new SqlCommand(sql, conn);
+
+            adap.InsertCommand = new SqlCommand(sql, conn);
+
+            try
+            {
+                adap.InsertCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627)
+                {
+                    Console.WriteLine("Workout already exists.");
+                    cmd.Dispose();
+                    conn.Close();
+                    return false;
+                }
+            }
+;
+            cmd.Dispose();
+            conn.Close();
+            return true;
+        }
+
+        internal static void Update()
+        {
+            string constr;
+
+            // for the connection to
+            // sql server database
+            SqlConnection conn;
+
+            // Data Source is the name of the
+            // server on which the database is stored.
+            // The Initial Catalog is used to specify
+            // the name of the database
+            // The UserID and Password are the credentials
+            // required to connect to the database.
+            constr = @"Data Source=localhost;Initial Catalog=WorkoutDatabase;User ID=sa;Password=t3ddy123";
+
+            conn = new SqlConnection(constr);
+
+            // to open the connection
+            conn.Open();
+
+            // use to perform read and write
+            // operations in the database
+            SqlCommand cmd;
+
+            // data adapter object is use to
+            // insert, update or delete commands
+            SqlDataAdapter adap = new SqlDataAdapter();
+
+            string sql = "";
+
+            // use the define sql
+            // statement against our database
+            sql = "update LegWorkouts set Name='Hip Thrust' where Name='Hip Thrusts'";
+
+            // use to execute the sql command so we
+            // are passing query and connection object
+            cmd = new SqlCommand(sql, conn);
+
+            // associate the insert SQL
+            // command to adapter object
+            adap.InsertCommand = new SqlCommand(sql, conn);
+
+            // use to execute the DML statement against
+            // our database
+            adap.InsertCommand.ExecuteNonQuery();
+
+            // closing all the objects
+            cmd.Dispose();
+            conn.Close();
+        }
+
+        internal static void Delete()
+        {
+            string constr;
+
+            // for the connection to
+            // sql server database
+            SqlConnection conn;
+
+            // Data Source is the name of the
+            // server on which the database is stored.
+            // The Initial Catalog is used to specify
+            // the name of the database
+            // The UserID and Password are the credentials
+            // required to connect to the database.
+            constr = @"Data Source=localhost;Initial Catalog=WorkoutDatabase;User ID=sa;Password=t3ddy123";
+
+            conn = new SqlConnection(constr);
+
+            // to open the connection
+            conn.Open();
+
+            // use to perform read and write
+            // operations in the database
+            SqlCommand cmd;
+
+            // data adapter object is use to
+            // insert, update or delete commands
+            SqlDataAdapter adap = new SqlDataAdapter();
+
+            string sql = "";
+
+            // use the define SQL statement
+            // against our database
+            sql = "delete from LegWorkouts where Name='Romanian Deadlift'";
+
+            // use to execute the sql command so we
+            // are passing query and connection object
+            cmd = new SqlCommand(sql, conn);
+
+            // associate the insert SQL
+            // command to adapter object
+            adap.InsertCommand = new SqlCommand(sql, conn);
+
+            // use to execute the DML statement
+            // against our database
+            adap.InsertCommand.ExecuteNonQuery();
+
+            // closing all the objects
+            cmd.Dispose();
+            conn.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Data.SqlClient;
+
+Dictionary<int, string> workoutTablesDict = new Dictionary<int, string>
+{
+    {1, "AbWorkouts"},
+    {2, "BackWorkouts"},
+    {3, "BicepWorkouts"},
+    {4, "ChestWorkouts"},
+    {5, "LegWorkouts"},
+    {6, "ShoulderWorkouts"},
+    {7, "TricepWorkouts"},
+};
+
+while (true)
+{
+    string? workoutTable = SelectSetofWorkouts(workoutTablesDict);
+
+    Console.WriteLine("-----------------------------------------\n");
+
+    EditWorkoutTable(workoutTable);
+
+}
+
+string SelectSetofWorkouts(Dictionary<int, string> workoutTablesDict)
+{
+    Console.WriteLine("Which set of workouts would you like to see? \n\nAbs (1) \nBack (2) \nBiceps (3) \nChest (4) \nLegs (5) \n" +
+            "Shoulders (6) \nTriceps (7) \n");
+
+    string? workoutTable = null;
+    while (workoutTable == null)
+    {
+        workoutTable = Console.ReadLine();
+        Console.WriteLine();
+        if (!int.TryParse(workoutTable, out int userInputInt) || userInputInt < 1 || userInputInt > 7)
+        {
+            Console.WriteLine("Not an accepted number!");
+            workoutTable = null;
+        }
+        else
+        {
+            workoutTable = workoutTablesDict[userInputInt];
+            Console.WriteLine("-----------------------------------------");
+            FitnessApp.Database.Read(workoutTable);
+        }
+    }
+
+    return workoutTable;
+}
+
+void EditWorkoutTable(string workoutTable)
+{
+    Console.WriteLine("What would you like to do? \n\nInsert new workout (1) \nRename existing workout (2) \n" +
+            "Delete existing workout (3) \nGo back (4) \n");
+
+    string? tableCommand = null;
+    while (tableCommand == null)
+    {
+        tableCommand = Console.ReadLine();
+        Console.WriteLine();
+        if (!int.TryParse(tableCommand, out int userInputInt) || userInputInt < 1 || userInputInt > 4)
+        {
+            Console.WriteLine("Not an accepted number!");
+            tableCommand = null;
+        }
+        else if (userInputInt == 1)
+        {
+            WorkoutInsertion(workoutTable);
+        }
+        else if (userInputInt == 2)
+        {
+            bool wasSuccessful = false;
+            string? workoutToBeUpdated = null;
+            string? workoutNewName = null;
+
+            while (workoutToBeUpdated == null)
+            {
+                Console.WriteLine("Enter workout name you'd like to update as appears in list.");
+                workoutToBeUpdated = Console.ReadLine();
+                Console.WriteLine();
+
+                if (string.IsNullOrWhiteSpace(workoutToBeUpdated))
+                {
+                    Console.WriteLine("Not a valid name!");
+                    workoutToBeUpdated = null;
+                }
+            }
+
+            while (workoutNewName == null)
+            {
+                Console.WriteLine("Enter new name of workout.");
+                workoutNewName = Console.ReadLine();
+                Console.WriteLine();
+
+                if (String.IsNullOrWhiteSpace(workoutNewName))
+                {
+                    Console.WriteLine("Not a valid name!");
+                    workoutNewName = null;
+                }
+            }
+
+
+
+        }
+        else if (userInputInt == 3)
+        {
+            FitnessApp.Database.Delete();
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
+void WorkoutInsertion(string workoutTable)
+{
+    bool wasSuccessful = false;
+    string? newWorkout = null;
+
+    while (newWorkout == null || wasSuccessful == false)
+    {
+        Console.WriteLine("Enter name of new workout to add");
+        newWorkout = Console.ReadLine();
+        Console.WriteLine();
+
+        if (string.IsNullOrWhiteSpace(newWorkout))
+        {
+            Console.WriteLine("Not a valid name!");
+            newWorkout = null;
+        }
+        else
+        {
+            wasSuccessful = FitnessApp.Database.Insert(workoutTable, newWorkout);
+        }
+    }
+}
